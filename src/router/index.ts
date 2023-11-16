a8267fe3b43d2a17d6671bce4e1c35b927da82ce
Dmitry Pozdnyakov <2903015@gmail.com>
2023-11-16 16:37:00 +0300

    v0.2

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,13 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import HomePage from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    name: 'Home',
+    component: () => import('@/views/Home.vue'),
   },
-  {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
-  }
 ]
 
 const router = createRouter({
